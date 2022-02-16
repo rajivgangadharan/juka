743a60a750add5f944cef69c4f1b83a4efe725a5
Rajiv Gangadharan <rajiv.gangadharan@gmail.com>
2022-02-16 11:21:11 +0530

    Corrected path of listissues.py

--- scripts/op_datapull.sh
+++ scripts/op_datapull.sh
@@ -1,6 +1,6 @@
-python3 listissues.py  --project OP \
+python3 ../listissues.py  --project OP \
 --query 'project = "OP" AND Type in ("Epic","Story") AND createdDate >= "2020/05/01"' \
 --output '../../DataSets/Opics_Delivery.csv' --max-rows 10000 --batch-size=25
-python3 listissues.py  --project OP \
+python3 ../listissues.py  --project OP \
 --query 'project = "OP" AND Type in ("Defect") AND createdDate >= "2020/05/01"' \
 --output '../../DataSets/Opics_Defects.csv' --max-rows 10000 --batch-size=25
