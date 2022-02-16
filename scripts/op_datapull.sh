221943cd8ff0e97b68f0b92272d61aa73fc22a47
Rajiv Gangadharan <rajiv.gangadharan@gmail.com>
2022-02-16 11:19:28 +0530

    Changed dates and file names in op_datapull.sh

--- scripts/op_datapull.sh
+++ scripts/op_datapull.sh
@@ -1,6 +1,6 @@
 python3 listissues.py  --project OP \
---query 'project = "OP" AND Type in ("Epic") AND createdDate >= "2020/01/01"' \
---output '../R/Opics_2021-06-17_Epics.csv' --max-rows 10000 --batch-size=25
+--query 'project = "OP" AND Type in ("Epic","Story") AND createdDate >= "2020/05/01"' \
+--output '../../DataSets/Opics_Delivery.csv' --max-rows 10000 --batch-size=25
 python3 listissues.py  --project OP \
---query 'project = "OP" AND Type in ("Defect") AND createdDate >= "2020/01/01"' \
---output '../R/Opics_2021-06-17_Defects.csv' --max-rows 10000 --batch-size=25
+--query 'project = "OP" AND Type in ("Defect") AND createdDate >= "2020/05/01"' \
+--output '../../DataSets/Opics_Defects.csv' --max-rows 10000 --batch-size=25
